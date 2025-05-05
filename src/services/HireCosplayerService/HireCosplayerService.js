520ca6d41a5ac53abb7f549d339079122bac55da
teadev2002 <anhhoanggg855@gmail.com>
2025-05-05 19:14:49 +0700

    hire cosplayer change api

--- src/services/HireCosplayerService/HireCosplayerService.js
+++ src/services/HireCosplayerService/HireCosplayerService.js
@@ -1,80 +1,3 @@
-// import { apiClient } from "../../api/apiClient.js";
-
-// const HireCosplayerService = {
-//   getAllCosplayers: async () => {
-//     try {
-//       const response = await apiClient.get("/api/Account/roleId/R004");
-//       // Đảm bảo dữ liệu trả về là mảng và có thuộc tính images
-//       return response.data.map((cosplayer) => ({
-//         ...cosplayer,
-//         images: cosplayer.images || [], // Nếu không có images, trả về mảng rỗng
-//       }));
-//     } catch (error) {
-//       console.error("Error fetching all cosplayers:", error);
-//       throw error;
-//     }
-//   },
-
-//   getAllCharacters: async () => {
-//     try {
-//       const response = await apiClient.get("/api/Character");
-//       return response.data;
-//     } catch (error) {
-//       throw new Error(
-//         error.response?.data?.message || "Lỗi khi lấy danh sách characters"
-//       );
-//     }
-//   },
-
-//   sendRequestHireCosplayer: async (requestData) => {
-//     try {
-//       const response = await apiClient.post("/api/Request", requestData);
-//       return response.data; // Trả về dữ liệu từ API (nếu có)
-//     } catch (error) {
-//       console.error("Error sending hire cosplayer request:", error);
-//       throw new Error(
-//         error.response?.data?.message || "Lỗi khi gửi yêu cầu thuê cosplayer"
-//       );
-//     }
-//   },
-
-//   // from RequestService
-//   getNameCosplayerInRequestByCosplayerId: async (accountId) => {
-//     try {
-//       const response = await apiClient.get(`/api/Account/${accountId}`);
-//       return response.data;
-//     } catch (error) {
-//       console.error("Error fetching cosplayer:", error);
-//       throw error;
-//     }
-//   },
-//   getHistoryByAccountId: async (accountId) => {
-//     try {
-//       const response = await apiClient.get(
-//         `/api/Request/GetAllRequestByAccount?accountId=${accountId}`
-//       );
-//       return response.data;
-//     } catch (error) {
-//       console.error("Error fetching history:", error);
-//       throw error;
-//     }
-//   },
-//   getAccountNoTaskByCharacterId: async (characterId, startDate, endDate) => {
-//     try {
-//       const response = await apiClient.get(
-//         `/api/Account/characterId?characterId=${characterId}&startDate=${startDate}&endDate=${endDate}`
-//       );
-//       return response.data;
-//     } catch (error) {
-//       console.error("Error fetching accounts:", error);
-//       throw error;
-//     }
-//   },
-// };
-
-// export default HireCosplayerService;
-
-//===========================================
 import { apiClient } from "../../api/apiClient.js";
 
 const HireCosplayerService = {
@@ -102,18 +25,6 @@ const HireCosplayerService = {
       );
     }
   },
-
-  // sendRequestHireCosplayer: async (requestData) => {
-  //   try {
-  //     const response = await apiClient.post("/api/Request", requestData);
-  //     return response.data; // Trả về dữ liệu từ API (nếu có)
-  //   } catch (error) {
-  //     console.error("Error sending hire cosplayer request:", error);
-  //     throw new Error(
-  //       error.response?.data?.message || "Lỗi khi gửi yêu cầu thuê cosplayer"
-  //     );
-  //   }
-  // },
 
   // from RequestService
   getNameCosplayerInRequestByCosplayerId: async (accountId) => {
@@ -168,6 +79,15 @@ const HireCosplayerService = {
         error.response?.data?.message ||
           "Lỗi khi lấy tài khoản theo tên nhân vật và ngày"
       );
+    }
+  },
+  ChangeCosplayer: async (data) => {
+    try {
+      const response = await apiClient.post(`/api/Account/characterId`, data);
+      return response.data;
+    } catch (error) {
+      console.error("Error changing cosplayer:", error);
+      throw error;
     }
   },
 
